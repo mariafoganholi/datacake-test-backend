0fca40a3333cee8ddee5dab17458bbe29ff233bb
mariafoganholi <mariaeduardafcf@hotmail.com>
2025-02-11 14:46:38 -0300

    Adiciona método PUT

--- todo/views.py
+++ todo/views.py
@@ -10,7 +10,7 @@ from rest_framework.decorators import api_view
 
 
 @api_view(['GET', 'POST'])
-def todo_list(request):
+def handle_todo_list(request):
     if request.method == 'GET':
         todo_items = TodoItem.objects.all()
         todoitem_serializer = TodoItemSerializer(todo_items, many=True)
@@ -19,7 +19,6 @@ def todo_list(request):
     if request.method == 'POST':
         todoitem_data = JSONParser().parse(request)
         todoitem_serializer = TodoItemSerializer(data=todoitem_data)
-        print(todoitem_serializer.is_valid())
         if not todoitem_serializer.is_valid():
             return JsonResponse(todoitem_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         todoitem_serializer.save()
@@ -27,21 +26,20 @@ def todo_list(request):
  
  
 @api_view(['PUT', 'DELETE'])
-def todo_item(request, pk):
-    print("tutorial detail")
-    # try: 
-    #     tutorial = TodoItem.objects.get(pk=pk) 
-    # except TodoItem.DoesNotExist: 
-    #     return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND) 
- 
-    # if request.method == 'PUT': 
-    #     tutorial_data = JSONParser().parse(request) 
-    #     tutorial_serializer = TodoItemSerializer(tutorial, data=tutorial_data) 
-    #     if tutorial_serializer.is_valid(): 
-    #         tutorial_serializer.save() 
-    #         return JsonResponse(tutorial_serializer.data) 
-    #     return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
- 
-    # elif request.method == 'DELETE': 
-    #     tutorial.delete() 
-    #     return JsonResponse({'message': 'Tutorial was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+def handle_todo_item(request, pk):
+    try:
+        todoitem = TodoItem.objects.get(pk=pk)
+    except TodoItem.DoesNotExist:
+        return JsonResponse({'message': 'Esta tarefa não existe'}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'DELETE':
+        todoitem.delete()
+        return JsonResponse({'message': 'Tarefa excluída com sucesso!'}, status=status.HTTP_204_NO_CONTENT)
+
+    if request.method == 'PUT':
+        changes = JSONParser().parse(request)
+        todoitem_serializer = TodoItemSerializer(todoitem, data=changes)
+        if not todoitem_serializer.is_valid():
+            return JsonResponse(todoitem_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        todoitem_serializer.save()
+        return JsonResponse(todoitem_serializer.data, status=status.HTTP_200_OK)
